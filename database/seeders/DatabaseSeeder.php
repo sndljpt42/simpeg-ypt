56a076d72800d2abbc0d2ee0a606621029f02316
sndljpt42 <marwanali941@gmail.com>
2026-07-23 11:27:28 +0700

    tambah jabatan akademik seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,6 +29,7 @@ class DatabaseSeeder extends Seeder
             JenisPegawaiSeeder::class,
             StatusPegawaiSeeder::class,
             GolonganSeeder::class,
+            JabatanAkademikSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/JabatanAkademikSeeder.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\JabatanAkademik;
+use App\Models\Golongan;
+
+class JabatanAkademikSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //Data Master Jabatan Akademik
+        $data = [
+            [
+                'nama'      => 'Asisten Ahli',
+                'gol_min'   => 'III/B',
+                'gol_max'   => 'III/B',
+                'usia'      => 65,
+                'maks_kgb'  => 2,
+            ],
+
+            [
+                'nama'      => 'Lektor',
+                'gol_min'   => 'III/C',
+                'gol_max'   => 'III/D',
+                'usia'      => 65,
+                'maks_kgb'  => 2,
+            ],
+
+            [
+                'nama'      => 'Lektor Kepala',
+                'gol_min'   => 'IV/A',
+                'gol_max'   => 'IV/C',
+                'usia'      => 65,
+                'maks_kgb'  => 2,
+            ],
+
+            [
+                'nama'      => 'Guru Besar',
+                'gol_min'   => 'IV/D',
+                'gol_max'   => 'IV/E',
+                'usia'      => 70,
+                'maks_kgb'  => null,
+            ],
+        ];
+
+        //simpan ke db
+
+        foreach($data as $item){
+            //cari id gol min
+            $golMin = Golongan::where('kode', $item['gol_min'])->first();
+            //cari id gol max
+            $golMax = Golongan::where('kode', $item['gol_max'])->first();
+
+            //jika salah satu golongan tidak ditemukan,
+            //jngan lanjutkan agar tidak error.
+
+            if(!$golMin || !$golMax){
+                $this->command-warn(
+                    "Golongan {$item['gol_min']} atau {$item['gol_max']} belum ada."
+                );
+            };
+
+            continue;
+        }
+
+        JabatanAkademik::updateOrCreate(
+            ['nama' => $item['nama']],
+
+            [
+                'golongan_min_id' => $golMin->id,
+                'golongan_max_id' => $golMax->id,
+                'usia_pensiun'    => $item['usia'],
+                'maks_kgb_setelah_mentok' => $item['maks_kgb'],
+            ]
+        );
+    }
+}
