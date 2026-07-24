7e0dfbae6d3d2ab8837e4d1741742b38ab418a34
sndljpt42 <marwanali941@gmail.com>
2026-07-24 14:23:51 +0700

    feat: add pegawai controllers and routing foundation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\TendikController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('dosen', DosenController::class);
+Route::resource('tendik', TendikController::class);
